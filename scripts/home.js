0f7bb6a38a21cf14e19e00a01ca75e6982cca4ff
gotayjosue <gotayjosue@gmail.com>
2025-01-12 21:41:40 -0600

    mobile view completed

--- scripts/home.js
+++ scripts/home.js
@@ -6,3 +6,13 @@ document.getElementById('currentyear').textContent = `${copyrightSymbol}${curren
 document.getElementById('name').textContent = `Josué Gotay `;
 document.getElementById('country').textContent = `Honduras`;
 document.getElementById('lastModified').textContent = `Last modification ${document.lastModified}`;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navigation = document.querySelector(".navigation");
+
+    menuToggle.addEventListener("click", () => {
+        menuToggle.classList.toggle("active")
+        navigation.classList.toggle("menu-open");
+    });
+});
