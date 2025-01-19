928d38d9687b8c39a4b5da40330feb0f4022f401
gotayjosue <gotayjosue@gmail.com>
2025-01-18 23:16:23 -0600

    json file and footer added

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,3 @@
+const year = new Date().getFullYear()
+const lastModified = document.querySelector('#modificationDate').textContent = `Last Modification: ${document.lastModified}`
+const name = document.querySelector('#name').textContent = `\u00A9 ${year} Trujillo Chamber of Commerce`
