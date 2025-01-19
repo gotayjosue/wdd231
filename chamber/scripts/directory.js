29b4bda454f6f6cc8bb9ec65d0bbdf8516910b2a
gotayjosue <gotayjosue@gmail.com>
2025-01-19 02:01:36 -0600

    members list added

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,52 @@
 const year = new Date().getFullYear()
 const lastModified = document.querySelector('#modificationDate').textContent = `Last Modification: ${document.lastModified}`
-const name = document.querySelector('#name').textContent = `\u00A9 ${year} Trujillo Chamber of Commerce`
+const name = document.querySelector('#name').textContent = `\u00A9 ${year} Trujillo Chamber of Commerce`;
+
+const membersContainer = document.querySelector('#members');
+
+let membersList = [];
+
+const getMembers = async () => {
+    
+    const response = await fetch("https://gotayjosue.github.io/wdd231/chamber/data/members.json")
+
+    membersList = await response.json();
+
+    displayMembers(membersList);
+}
+
+function displayMembers(members){
+
+    members.forEach(member => {
+
+        const section = document.createElement('section')
+        const title = document.createElement('h3')
+        const tag = document.createElement('p')
+        tag.className = "tag"
+
+        const img = document.createElement('img')
+        img.src = member.imageUrl
+        img.alt = member.name
+
+        const phone = document.createElement('p')
+        phone.className = "phone"
+        const site = document.createElement('a')
+        site.className = "site"
+
+        title.textContent = member.name
+        tag.textContent = member.bussinesTag
+        phone.innerHTML = `<strong>Phone:</strong> ${member.phone}`
+        site.innerHTML = `<strong>URL:</strong> ${member.website}`
+
+        section.appendChild(title)
+        section.appendChild(tag)
+        section.appendChild(img)
+        section.appendChild(phone)
+        section.appendChild(site)
+
+        membersContainer.appendChild(section)
+    });
+
+};
+
+getMembers()
