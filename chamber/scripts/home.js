9ba91d9880a7bbb409c268877d95b6f9b19454b2
gotayjosue <gotayjosue@gmail.com>
2025-01-25 19:23:42 -0600

    himidity percentage sign

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -159,7 +159,7 @@ function displayResults(data){
     weatherContainer.appendChild(icon)
     const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
     description.textContent = capitalize(data.weather[0].description)
-    humidity.innerHTML = `<strong>Humidity:</strong> ${data.main.humidity}`
+    humidity.innerHTML = `<strong>Humidity:</strong> ${data.main.humidity}%`
     high.innerHTML = `<strong>High:</strong> ${data.main.temp_max} ºC`
     low.innerHTML = `<strong>Low:</strong> ${data.main.temp_min} ºC`
     sunrise.innerHTML = `<strong>Sunrise:</strong> ${formatTime(sunriseHour)}`
