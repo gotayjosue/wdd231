bbef468a70e531432dcd6942f2f0091a9cb5ea00
gotayjosue <gotayjosue@gmail.com>
2025-02-01 22:28:49 -0600

    modals and form finished

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -28,3 +28,57 @@ modeButton.addEventListener('click', () =>{
     document.body.classList.toggle('black')
 
 });
+
+//Selecting modal elements
+const nonProfitButton = document.querySelector('#nonButton')
+const bronzeButton = document.querySelector('#bronzeButton')
+const silverButton = document.querySelector('#silverButton')
+const goldButton = document.querySelector('#goldButton')
+
+const nonProfitModal = document.querySelector('#nonModal')
+const bronzeModal = document.querySelector('#bronzeModal')
+const silverModal = document.querySelector('#silverModal')
+const goldModal = document.querySelector('#goldModal')
+
+const nonCloseButton = document.querySelector('#noneCloseButton')
+const bronzeCloseButton = document.querySelector('#bronzeCloseButton')
+const silverCloseButton = document.querySelector('#silverCloseButton')
+const goldCloseButton = document.querySelector('#goldCloseButton')
+
+//Non Profit membership modal fuctions
+nonProfitButton.addEventListener('click', () => {
+    nonProfitModal.showModal()
+})
+
+nonCloseButton.addEventListener('click', () => {
+    nonProfitModal.close()
+})
+
+//Bronze membership modal fuctions
+bronzeButton.addEventListener('click', () => {
+    bronzeModal.showModal()
+})
+
+bronzeCloseButton.addEventListener('click', () => {
+    bronzeModal.close()
+})
+
+
+//Silver membership modal fuctions
+silverButton.addEventListener('click', () => {
+    silverModal.showModal()
+})
+
+silverCloseButton.addEventListener('click', () => {
+    silverModal.close()
+})
+
+
+//Gold membership modal fuctions
+goldButton.addEventListener('click', () => {
+    goldModal.showModal()
+})
+
+goldCloseButton.addEventListener('click', () => {
+    goldModal.close()
+})
